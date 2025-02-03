7926d11a3d30f0414b5251fd2707da73a83c49b0
ralphyhlam <ralphyhlam@uchicago.edu>
2025-02-03 20:12:23 +0000

    set up initial page

--- app.rb
+++ app.rb
@@ -1,9 +1,21 @@
 require "sinatra"
 require "sinatra/reloader"
+require "dotenv/load"
+require "http"
+require "json"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  template = "https://api.exchangerate.host/list?access_key="
+  list_url = "https://api.exchangerate.host/list?access_key=#{ENV.fetch("EXCHANGE_API")}"
+  @list_results= HTTP.get(list_url).to_s
+  @list_format = JSON.parse(@list_results)
+  currencies = @list_format.fetch("currencies")
+  @list_currencies = currencies.keys
+  erb(:homepage)
+end
+
+get("/:currency_choice") do 
+  @choice = params.fetch("currency_choice")
+
+  erb(:conversion_choices)
 end
