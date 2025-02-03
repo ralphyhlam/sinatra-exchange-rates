197d30ed84f01116c3b6ce742e79683c3d24b9d9
ralphyhlam <ralphyhlam@uchicago.edu>
2025-02-03 22:16:34 +0000

    finished --> fixed smaller formatting errors

--- app.rb
+++ app.rb
@@ -5,7 +5,6 @@ require "http"
 require "json"
 
 get("/") do
-  template = "https://api.exchangerate.host/list?access_key="
   list_url = "https://api.exchangerate.host/list?access_key=#{ENV.fetch("EXCHANGE_API")}"
   @list_results= HTTP.get(list_url).to_s
   @list_format = JSON.parse(@list_results)
@@ -15,7 +14,25 @@ get("/") do
 end
 
 get("/:currency_choice") do 
-  @choice = params.fetch("currency_choice")
-
+  @choice = params.fetch("currency_choice").to_s
+  list_url = "https://api.exchangerate.host/list?access_key=#{ENV.fetch("EXCHANGE_API")}"
+  @list_results= HTTP.get(list_url).to_s
+  @list_format = JSON.parse(@list_results)
+  currencies = @list_format.fetch("currencies")
+  @list_currencies = currencies.keys
   erb(:conversion_choices)
+end
+
+get("/:currency_choice/:conversion_choice") do 
+  @choice = params.fetch("currency_choice").to_s
+  @target = params.fetch("conversion_choice").to_s
+
+  @conversion_url = "https://api.exchangerate.host/convert?from=#{@choice}&to=#{@target}&amount=1&access_key=#{ENV.fetch("EXCHANGE_API")}"
+  conversion_results = HTTP.get(@conversion_url)
+  @conversion_format = JSON.parse(conversion_results)
+
+  info = @conversion_format.fetch("info")
+  @quote = info.fetch("quote")
+
+  erb(:converted)
 end
